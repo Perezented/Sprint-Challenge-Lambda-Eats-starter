16bf0b163a28cfa8092e7b5d51c2668a8ebfa61b
Perezented <57721734+Perezented@users.noreply.github.com>
2020-03-27 15:35:04 -0500

    added the rules to the separate page.

--- cypress/integration/login_test.js
+++ cypress/integration/login_test.js
@@ -2,14 +2,15 @@ describe("A test for Single Page Applications Sprint", function() {
     it("Visiting the login in the app", function() {
         cy.visit("http://localhost:3000/login");
     });
-    it("Does not allow empty fields", function() {
+    it("Checks for empty fields", function() {
         //name
-        cy.get(`input[id='name']`).should("have.length", 1);
+        cy.get(`input[id='name']`).should("be.empty");
         //email
-        cy.get(`input[id='email']`).should("have.length", 1);
+        cy.get(`input[id='email']`).should("be.empty");
         //password
-        cy.get(`input[id='password']`).should("have.length", 1);
+        cy.get(`input[id='password']`).should("be.empty");
     });
+
     it("Add test to inputs and submit login", function() {
         //name
         cy.get(`input[id='name']`)
@@ -27,6 +28,15 @@ describe("A test for Single Page Applications Sprint", function() {
         cy.get(`[type='checkbox']`)
             .check()
             .should("be.checked"); //button
+    });
+
+    it("Second check for empty fields", function() {
+        expect(`[id='name']`).to.have.lengthOf.at.least(2);
+        expect(`[id='email']`).to.have.lengthOf.at.least(2);
+        expect(`[id='password']`).to.have.lengthOf.at.least(2);
+    });
+
+    it("Will now submit the form to see results", function() {
         cy.get(`[id='loginSubmit']`).click();
     });
 });
