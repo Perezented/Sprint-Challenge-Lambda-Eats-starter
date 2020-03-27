abec5f093b9ac3e89a702a355ebd9eafd5fb048a
Perezented <57721734+Perezented@users.noreply.github.com>
2020-03-27 12:30:48 -0500

    added a login form, altho it is just copy and paste from the user oing for it should be quite as easy haha.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Route, Link } from "react-router-dom";
 import TopBanner from "./components/TopBanner";
 import Form from "./components/PizzaForm";
 import Home from "./components/Home";
+import Login from "./components/Login";
 const App = () => {
     return (
         <>
             <TopBanner />
+            <Route exact path="/login" component={Login} />
             <div>
-                {" "}
                 <Route exact path="/">
                     <Home />
                 </Route>
