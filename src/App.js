92c601cf8cf88150fcfc532c8b08f887f378749f
Perezented <57721734+Perezented@users.noreply.github.com>
2020-03-27 12:15:44 -0500

    Added a new Home.js. The other one got made into the top banner area with the logo and links/buttons.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import Home from "./components/Home";
+import { Route, Link } from "react-router-dom";
+import TopBanner from "./components/TopBanner";
 import Form from "./components/PizzaForm";
+
 const App = () => {
     return (
         <>
-            <Home />
-            <Form />
+            <TopBanner />
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <p>Home of the square pizza made to fit in your pizza box!</p>
+            <p>Order your pizza here!</p>
+
+            <Route path="/pizza/" component={Form} />
         </>
     );
 };
---
+++ src/components/TopBanner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./Nav";
+
+export default function TopBanner() {
+    return (
+        <section>
+            <Nav />
+            <h1>Lambda Eats</h1>
+        </section>
+    );
+}
